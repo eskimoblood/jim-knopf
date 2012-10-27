6f0ff1164ca69e194bc3853e0e8bbcda650c2275
Andreas Köberle <koeberle@gmail.com>
2012-10-28 00:14:24 +0200

    add test for key events

--- test/knob-test.js
+++ test/knob-test.js
@@ -6,6 +6,7 @@ describe("Knob", function() {
     "use strict";
     var div = document.createElement('div');
     div.innerHTML = '<input disabled id="test" type="range" max="200" min="100" data-width="50" data-height="60" data-angleOffset="220" data-angleRange="280" data-startColor="#ffea05" data-endColor="#fc005c">';
+
     var input = div.getElementsByTagName('input')[0];
     var init = sinon.spy();
     var update = sinon.spy();
@@ -17,7 +18,6 @@ describe("Knob", function() {
     var knob = new Knob(input, ui);
     assert.equals(init.args[0][0], input.parentNode);
     var settings = init.args[0][1];
-    console.log(settings);
     assert.equals(settings.max, 200);
     assert.equals(settings.min, 100);
     assert.equals(settings.width, 50);
@@ -28,5 +28,43 @@ describe("Knob", function() {
     assert.equals(settings.endcolor, '#fc005c');
   });
 
+  it("handle key down", function() {
+    var div = document.createElement('div');
+    div.innerHTML = '<input disabled id="test" type="range" max="200" min="100" step="10" data-width="50" data-height="60" data-angleOffset="220" data-angleRange="280" data-startColor="#ffea05" data-endColor="#fc005c">';
+    var input = div.getElementsByTagName('input')[0];
+    var init = sinon.spy();
+    var update = sinon.spy();
+    var ui = {
+      init: init,
+      update: update
+    };
+    var knob = new Knob(input, ui);
+    keyDown(input.parentNode, 38);
+    checkUpdate(update, 0.6, 160);
+
+    keyDown(input.parentNode, 39);
+    checkUpdate(update, 0.7, 170);
+
+    keyDown(input.parentNode, 40);
+    checkUpdate(update, 0.6, 160);
+
+    keyDown(input.parentNode, 37);
+    checkUpdate(update, 0.5, 150);
+  });
+
+  function checkUpdate(update, expectedPercent, expectedValue) {
+    var percent = update.lastCall.args[0];
+    var value = update.lastCall.args[1];
+    assert.equals(expectedPercent, percent);
+    assert.equals(expectedValue, value);
+  }
+
+  function keyDown(el, keycode) {
+    var eventObj = document.createEvent("Events");
+    eventObj.initEvent("keydown", true, true);
+    eventObj.keyCode = keycode;
+    eventObj.shiftKey = false;
+    el.dispatchEvent(eventObj);
+  }
 
 });
