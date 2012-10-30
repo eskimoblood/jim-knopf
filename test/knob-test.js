9613190c0960ba70fdbb89cb6324a47ae2cafab8
Andreas Köberle <koeberle@gmail.com>
2012-10-30 22:18:29 +0100

    add bind shim, cause phantomjs support this yet

--- test/knob-test.js
+++ test/knob-test.js
@@ -1,21 +1,55 @@
 buster.spec.expose(); // Make some functions global
 
+//TODO remove when phantomJS supports bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== "function") {
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+      fToBind = this,
+      fNOP = function() {},
+      fBound = function() {
+        return fToBind.apply(this instanceof fNOP && oThis
+          ? this
+          : oThis,
+          aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 describe("Knob", function() {
 
-  it("pass the value of the input to the Ui", function() {
+  var init;
+  var update;
+  var input;
+  var knob;
+
+  before(function() {
     "use strict";
     var div = document.createElement('div');
-    div.innerHTML = '<input disabled id="test" type="range" max="200" min="100" data-width="50" data-height="60" data-angleOffset="220" data-angleRange="280" data-startColor="#ffea05" data-endColor="#fc005c">';
+    div.innerHTML = '<input disabled id="test" type="range" max="200" min="100" step="10" data-width="50" data-height="60" data-angleOffset="220" data-angleRange="280" data-startColor="#ffea05" data-endColor="#fc005c">';
 
-    var input = div.getElementsByTagName('input')[0];
-    var init = sinon.spy();
-    var update = sinon.spy();
+    input = div.getElementsByTagName('input')[0];
+    init = sinon.spy();
+    update = sinon.spy();
+
     var ui = {
       init: init,
       update: update
     };
 
-    var knob = new Knob(input, ui);
+    knob = new Knob(input, ui);
+  });
+
+  it("pass the value of the input to the Ui", function() {
+
     assert.equals(init.args[0][0], input.parentNode);
     var settings = init.args[0][1];
     assert.equals(settings.max, 200);
@@ -28,28 +62,50 @@ describe("Knob", function() {
     assert.equals(settings.endcolor, '#fc005c');
   });
 
-  it("handle key down", function() {
-    var div = document.createElement('div');
-    div.innerHTML = '<input disabled id="test" type="range" max="200" min="100" step="10" data-width="50" data-height="60" data-angleOffset="220" data-angleRange="280" data-startColor="#ffea05" data-endColor="#fc005c">';
-    var input = div.getElementsByTagName('input')[0];
-    var init = sinon.spy();
-    var update = sinon.spy();
-    var ui = {
-      init: init,
-      update: update
-    };
-    var knob = new Knob(input, ui);
-    keyDown(input.parentNode, 38);
-    checkUpdate(update, 0.6, 160);
+  describe('on press ', function() {
 
-    keyDown(input.parentNode, 39);
-    checkUpdate(update, 0.7, 170);
+    it("the UP key value increase by step", function() {
+      keyDown(input.parentNode, 38);
+      checkUpdate(update, 0.6, 160);
+    });
 
-    keyDown(input.parentNode, 40);
-    checkUpdate(update, 0.6, 160);
+    it("the RIGHT key value increase by step", function() {
+      keyDown(input.parentNode, 39);
+      checkUpdate(update, 0.6, 160);
+    });
 
-    keyDown(input.parentNode, 37);
-    checkUpdate(update, 0.5, 150);
+    it("the DOWN key value decrease by step", function() {
+      keyDown(input.parentNode, 40);
+      checkUpdate(update, 0.4, 140);
+    });
+
+    it("the LEFT key value decrease by step", function() {
+      keyDown(input.parentNode, 37);
+      checkUpdate(update, 0.4, 140);
+    });
+  });
+
+  describe('on move the mouse wheel', function() {
+
+    it('up it decrease by step', function() {
+      mouseWheelMove(input.parentNode, 'Y', -10);
+      checkUpdate(update, 0.4, 140);
+    });
+
+    it('down it increase by step', function() {
+      mouseWheelMove(input.parentNode, 'Y', 10);
+      checkUpdate(update, 0.6, 160);
+    });
+
+    it('left it decrease by step', function() {
+      mouseWheelMove(input.parentNode, 'X', -10);
+      checkUpdate(update, 0.4, 140);
+    });
+
+    it('right it increase by step', function() {
+      mouseWheelMove(input.parentNode, 'X', 10);
+      checkUpdate(update, 0.6, 160);
+    });
   });
 
   function checkUpdate(update, expectedPercent, expectedValue) {
@@ -64,6 +120,13 @@ describe("Knob", function() {
     eventObj.initEvent("keydown", true, true);
     eventObj.keyCode = keycode;
     eventObj.shiftKey = false;
+    el.dispatchEvent(eventObj);
+  }
+
+  function mouseWheelMove(el, direction, value) {
+    var eventObj = document.createEvent("Events");
+    eventObj.initEvent("mousewheel", true, true);
+    eventObj['wheelDelta' + direction] = value;
     el.dispatchEvent(eventObj);
   }
 
